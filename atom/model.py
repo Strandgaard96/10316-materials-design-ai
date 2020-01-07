c3f4fd92e331d1260be9864119c77be2aec9b045
s153327 <s153327@student.dtu.dk>
2020-01-07 13:08:32 +0100

    missing calculate

--- atom/model.py
+++ atom/model.py
@@ -15,4 +15,19 @@ class LennartJonesModel:
         REturn two values, The first is the enegy,
         the first is the force on all atoms"""
 
+    # Check same type elements
+    for elem in system
+        if system.sym != self.element:
+            raise ValueError("Elements must be equal")
+
+    E = 0
+    F = np.zeros(len(system))
+    for elem in system:
+        for elem2 in system:
+            r = elem.pos - elem2.pos
+            E += 4*self.epsilon*((self.sigma/r)^12-(self.sigma/r)^6)
+            F[i,j] += 4*self.epsilon*(12*(self.epsilon/r)^12-6*(self.epsilon/r)^6)*(r/r^2)
+
+
+
 
