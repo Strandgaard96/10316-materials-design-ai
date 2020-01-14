1a48f8fa4e7e231e56acb592b3926cc13f2bae13
s153327 <s153327@student.dtu.dk>
2020-01-14 12:37:11 +0100

    working fit

--- Mini-project/main.py
+++ Mini-project/main.py
@@ -85,56 +85,79 @@ elemdict={'Ag':[5,11],
 
 energies = [s.heat_of_formation_all for s in syss]
 
-hist=plt.hist(energies,bins=100)
-
-# Hyperparameters
+#hist=plt.hist(energies,bins=100)
 lam = 10E-4
-l = 1
+l = 0.5
 sigma = 0
-k_0 = 1
+
+def find_k0(yp,prod):
+    k_0 = (1/(len(yp))*np.transpose(yp).dot(prod))
+    return k_0
 
 # Construction of k
-def kvec(x,nodes):
+def kvec(x,nodes,k_0):
     k = np.zeros(len(nodes))
     for i in range(len(nodes)):
         k[i] = k_0*math.exp(-(np.linalg.norm(x-nodes[i,:]))**2/(2*l**2))
     return k
 
 def construct_C(data):
-    C = np.zeros((data.shape[0], data.shape[1]))
+    C = np.zeros((data.shape[0], data.shape[0]))
     for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            C[i, j] = k_0*math.exp(-(np.linalg.norm(data[i,:]-data[j,:])) ** 2 / (2 * l ** 2))
+        for j in range(data.shape[0]):
+            C[i, j] = math.exp(-(np.linalg.norm(data[i,:]-data[j,:])) ** 2 / (2 * l ** 2))
     np.fill_diagonal(C, C.diagonal() + sigma**2)
     return C
 
 def calculate_fit(nodes, x_tofit,yp):
-    y_fit = np.zeros(len(nodes))
+    y_fit = np.zeros(x_tofit.shape[0])
     C = construct_C(nodes)
+    prod = np.linalg.inv(C).dot(yp)
+    k_0 = find_k0(yp,prod)
+    C = k_0*C
+    prod = np.linalg.inv(C).dot(yp)
 
     for i in range(x_tofit.shape[0]):
-        k = kvec(x_tofit[i,:], nodes)
-        y_fit[i] = np.transpose(k).dot(np.linalg.inv(C).dot(yp))
-        return y_fit
+        k = kvec(x_tofit[i,:], nodes,k_0)
+        y_fit[i] = np.transpose(k).dot(prod)
+    return y_fit
 
 def costruct_training(syss,elemdict, aniondict):
-    x = np.empty(8)
-    energy = np.zeros(20000)
+    x = np.zeros((1, 8))
+    energy = np.zeros(1)
     for i,elem in enumerate(syss):
         j = random.randrange(0,100);
-        if j< 3:
-            x = np.vstack([x,elemdict[elem.A_ion] + elemdict[elem.B_ion] + aniondict[elem.anion]])
-            energy[i] = elem.heat_of_formation_all
+        if j<= 5:
+            x = np.vstack([x, elemdict[elem.A_ion] + elemdict[elem.B_ion] + aniondict[elem.anion]])
+            energy = np.concatenate((energy, [elem.heat_of_formation_all]), axis=0)
     x = np.delete(x, 0, 0)
-    energy  = energy[energy != 0]
+    energy = np.delete(energy,0,0)
+    return x,energy
+
+def costruct_test2(syss,elemdict, aniondict):
+    x = np.zeros((1, 8))
+    energy = np.zeros(1)
+    for i,elem in enumerate(syss):
+        j = random.randrange(0,100);
+        if j<= 2:
+            x = np.vstack([x, elemdict[elem.A_ion] + elemdict[elem.B_ion] + aniondict[elem.anion]])
+            energy = np.concatenate((energy, [elem.heat_of_formation_all]), axis=0)
+    x = np.delete(x, 0, 0)
+    energy = np.delete(energy,0,0)
     return x,energy
 
 def costruct_test():
     x_tofit = np.array([[1,2,3,4,7,2,1,8],[1,2,3,4,5,6,7,8]])
     return x_tofit
 
+def construct_error(prediction, actual):
+    error = (abs(actual-prediction))/(abs(1+actual))*100
+    plt.plot(error)
+    return error
+
 
 if __name__ == '__main__':
     xp,energy = costruct_training(syss,elemdict,aniondict)
-    x_tofit = costruct_test()
+    x_tofit,energy_all = costruct_test2(syss,elemdict,aniondict)
     y_fit = calculate_fit(xp, x_tofit, energy)
+    error = construct_error(y_fit, energy_all)
